55ede0543a315459a5efa2834342054c71156715
Karl Wynn <karl.wynn2@dwp.gsi.gov.uk>
2019-06-06 15:11:40 +0100

    webapp

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
+import { Avatar } from "../components/avatar";
 
 type ProfileProps = RouteComponentProps;
 
 export const Profile: React.FunctionComponent<ProfileProps> = () => (
     <>
+
+        <Avatar/>
+
         <h2>Profile</h2>
 
         <Link to={`/`}>Back to Search</Link>
